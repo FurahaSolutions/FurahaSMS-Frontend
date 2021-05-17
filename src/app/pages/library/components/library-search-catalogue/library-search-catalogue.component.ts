62d0902a2bcd435f6093ace1c6cc3aebf82c37bb
Owen Kelvin <owen@furahasolutions.tech>
2021-05-17 12:52:25 +0300

    Fixed library catalogue search

--- src/app/pages/library/components/library-search-catalogue/library-search-catalogue.component.ts
+++ src/app/pages/library/components/library-search-catalogue/library-search-catalogue.component.ts
@@ -1,53 +1,74 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
-import {Store} from '@ngrx/store';
+import { ChangeDetectorRef, Component } from '@angular/core';
+import { Store } from '@ngrx/store';
 import * as fromStore from '../../../../store/reducers';
-import {FormBuilder, FormControl, FormGroup} from '@angular/forms';
-import {LibraryAuthorService} from '../../services/library-author.service';
-import {Observable} from 'rxjs';
-import {LibraryBookService} from '../../services/library-book.service';
-import {LibraryPublisherService} from '../../services/library-publisher.service';
-import {takeWhile} from 'rxjs/operators';
-import {loadLibraryBooksSuccess} from '../../store/actions/library-book.actions';
+import { FormBuilder, FormControl } from '@angular/forms';
+import { LibraryAuthorService } from '../../services/library-author.service';
+import { BehaviorSubject, Observable, Observer, of } from 'rxjs';
+import { LibraryBookService } from '../../services/library-book.service';
+import { LibraryPublisherService } from '../../services/library-publisher.service';
+import { switchMap, tap } from 'rxjs/operators';
+import { loadLibraryBooksSuccess } from '../../store/actions/library-book.actions';
+import { formMixin } from "../../../../shared/mixins/form.mixin";
 
 @Component({
   selector: 'app-library-search-catalogue',
   templateUrl: './library-search-catalogue.component.html',
-  styleUrls: ['./library-search-catalogue.component.css']
+  styleUrls: ['./library-search-catalogue.component.css'],
 })
-export class LibrarySearchCatalogueComponent implements OnInit, OnDestroy {
-  searchParamsForm: FormGroup;
-  authors$: Observable<any>;
-  books$: Observable<any[]>;
-  publishers$: Observable<any>;
-  titles$: Observable<any>;
-  isSubmitting: boolean;
-  books: any[];
-  componentIsActive: boolean;
-  bookSearched: boolean;
+export class LibrarySearchCatalogueComponent extends formMixin() {
+  searchParamsForm = this.fb.group({
+    title: [''],
+    author: [''],
+    publisher: [''],
+  });
+
+  authors$ = new Observable((observer: Observer<string>) => {
+    observer.next(this.author.value);
+  }).pipe(
+    switchMap(search => search ? this.authorsService.filter(search) : of([])),
+  );
+
+  publishers$ = new Observable((observer: Observer<string>) => {
+    observer.next(this.publisher.value);
+  }).pipe(
+    switchMap(search => search ? this.publisherService.filter(search) : of([])),
+  );
+  titles$ = new Observable((observer: Observer<string>) => {
+    observer.next(this.title.value);
+  }).pipe(
+    switchMap(search => search ? this.booksService.filter({title: search}) : of([])),
+  );
+
+  bookSearchedSubject$ = new BehaviorSubject(false);
+
+  books$ = this.bookSearchedSubject$.pipe(
+    tap(searched => {
+      if(searched) this.submitInProgressSubject$.next(true);
+    }),
+    switchMap(x => x ? this.booksService.filter(this.searchParamsForm.value) : []),
+    tap((books) => {
+      this.store.dispatch(loadLibraryBooksSuccess({data: books}));
+      this.submitInProgressSubject$.next(false);
+    })
+  );
 
   constructor(
     private fb: FormBuilder, private store: Store<fromStore.AppState>,
     private authorsService: LibraryAuthorService,
     private booksService: LibraryBookService,
-    private publisherservice: LibraryPublisherService,
+    private publisherService: LibraryPublisherService,
+    private cdr: ChangeDetectorRef
   ) {
+    super();
   }
 
-  ngOnInit() {
-    this.componentIsActive = true;
-    this.bookSearched = false;
-    this.clearForm();
-    this.authors$ = this.authorsService.filter(this.author.value);
-    this.titles$ = this.booksService.filter({title: this.title.value});
-    this.publishers$ = this.publisherservice.filter(this.publisher.value);
-  }
 
   clearForm() {
-    this.searchParamsForm = this.fb.group({
-      title: [''],
-      author: [''],
-      publisher: [''],
-    });
+    this.searchParamsForm.setValue({
+      title: '',
+      author: '',
+      publisher: '',
+    })
   }
 
   get author(): FormControl {
@@ -62,24 +83,10 @@ export class LibrarySearchCatalogueComponent implements OnInit, OnDestroy {
     return this.searchParamsForm.get('title') as FormControl;
   }
 
-  submitSearchParamsForm() {
-    this.isSubmitting = true;
-    this.books$ = this.booksService.filter(this.searchParamsForm.value);
-    this.books$
-      .pipe(takeWhile(() => this.componentIsActive))
-      .subscribe({
-        next: books => {
-          this.books = books;
-          this.store.dispatch(loadLibraryBooksSuccess({data: books}));
-        },
-        complete: () => {
-          this.bookSearched = true;
-          this.isSubmitting = false;
-        }
-      });
-  }
 
-  ngOnDestroy() {
-    this.componentIsActive = false;
+  submitSearchParamsForm() {
+
+    this.bookSearchedSubject$.next(true);
+    this.cdr.detectChanges()
   }
 }
